c8ace8605372d3e2f0739b2adeca94560d910b9f
Evgeniy <Evgeniy>
2018-04-16 22:00:47 +0300

    fix filename

--- src/test/java/lab/test/AuthorizationTests.java
+++ src/test/java/lab/test/AuthorizationTests.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 
 import static org.junit.Assert.assertTrue;
 
-class AutorizationTests {
+class AuthorizationTests {
 
     SeleniumUtils utils = new SeleniumUtils();
 
